a11c5ec9e5a6fa9fac2a89158965d51ed64c6df7
Anastasia <anastasiiya.p@gmail.com>
2024-05-11 12:32:41 +0300

    Fix logic to pass tests

--- app/parse.py
+++ app/parse.py
@@ -1,6 +1,5 @@
 import csv
 import re
-import time
 from dataclasses import dataclass
 from selenium.webdriver.support import expected_conditions as EC
 from urllib.parse import urljoin
@@ -23,7 +22,6 @@ class Product:
     title: str
     description: str
     price: float
-    memory: int
     rating: int
     num_of_reviews: int
 
@@ -46,31 +44,33 @@ class Scraper:
         except TimeoutException:
             pass
 
-    def get_single_product(self, memory: int = None) -> Product:
+    def get_single_product(self, card) -> Product:
+        description = card.find_element(By.CLASS_NAME, "description").text
         price = float(
-            self.driver.find_element(By.CLASS_NAME, "price").text.replace(
-                "$", ""
-            )
+            card.find_element(By.CLASS_NAME, "price").text.replace("$", "")
+        )
+        rating = len(card.find_elements(By.CLASS_NAME, "ws-icon-star"))
+        num_of_reviews = int(
+            re.search(
+                r"\d+", card.find_element(By.CLASS_NAME, "review-count").text
+            ).group(0)
         )
 
+        title_link = card.find_element(
+            By.CSS_SELECTOR, "h4 > a.title"
+        ).get_attribute("href")
+
+        self.driver.get(title_link)
+
+        title = self.driver.find_element(By.CLASS_NAME, "card-title").text
+        self.driver.back()
+
         return Product(
-            title=self.driver.find_element(By.CLASS_NAME, "title").text,
-            description=self.driver.find_element(
-                By.CLASS_NAME, "description"
-            ).text,
+            title=title,
+            description=description,
             price=price,
-            memory=memory,
-            rating=len(
-                self.driver.find_elements(By.CLASS_NAME, "ws-icon-star")
-            ),
-            num_of_reviews=int(
-                re.search(
-                    r"\d+",
-                    self.driver.find_element(
-                        By.CLASS_NAME, "review-count"
-                    ).text,
-                ).group(0)
-            ),
+            rating=rating,
+            num_of_reviews=num_of_reviews,
         )
 
     def click_more_button(self):
@@ -79,71 +79,38 @@ class Scraper:
                 more_button = self.driver.find_element(
                     By.CLASS_NAME, "ecomerce-items-scroll-more"
                 )
+
                 self.driver.execute_script(
-                    "arguments[0].scrollIntoView();", more_button
+                    "arguments[0].click();", more_button
                 )
-                self.driver.execute_script("window.scrollBy(0, -100);")
+
                 more_button.click()
-            except (ElementNotInteractableException, NoSuchElementException):
+
+            except (
+                ElementNotInteractableException,
+                NoSuchElementException,
+                TimeoutException,
+            ):
                 break
-        # try:
-        #     more_button = self.driver.find_element(
-        #         By.CLASS_NAME, "ecomerce-items-scroll-more"
-        #     )
-        #
-        #     while more_button:
-        #         more_button.click()
-        #
-        # except ElementNotInteractableException:
-        #     pass
-        # except NoSuchElementException:
-        #     pass
 
-    def get_products_with_different_memory(
-        self,
-    ) -> list[Product]:
-        products = []
-
-        try:
-            swatches = self.driver.find_element(By.CLASS_NAME, "swatches")
-            buttons = swatches.find_elements(By.TAG_NAME, "button")
-
-            for button in buttons:
-                button.click()
-                product = self.get_single_product(
-                    int(button.get_attribute("value"))
-                )
-                products.append(product)
-                # time.sleep(1)
-
-        except NoSuchElementException:
-            product = self.get_single_product()
-            products.append(product)
-
-        return products
-
-    def get_all_products(self, page_url) -> list[Product]:
+    def get_products_list(self, page_url) -> list[Product]:
         self.driver.implicitly_wait(1)
         self.driver.get(page_url)
         self.accept_cookies()
         self.click_more_button()
 
-        page_links = self.driver.find_elements(By.CSS_SELECTOR, "h4 > a.title")
-        page_links_li = [link.get_attribute("href") for link in page_links]
-
         products = []
 
-        for link in page_links_li:
-            self.driver.get(link)
-            products += self.get_products_with_different_memory()
-        print("products", products)
+        products_cards = self.driver.find_elements(By.CLASS_NAME, "card-body")
+        for card in products_cards:
+            products.append(self.get_single_product(card))
 
         return products
 
     def write_products_to_csv(self, page_url, csv_file_name):
-        products = self.get_all_products(page_url)
+        products = self.get_products_list(page_url)
         with open(csv_file_name, "w", encoding="utf-8", newline="") as file:
-            writer = csv.writer(file, delimiter=";")
+            writer = csv.writer(file)
             writer.writerow(Product.__annotations__.keys())
 
             for product in products:
@@ -152,7 +119,6 @@ class Scraper:
                         product.title,
                         product.description,
                         product.price,
-                        product.memory,
                         product.rating,
                         product.num_of_reviews,
                     ]
@@ -162,7 +128,7 @@ class Scraper:
         self.driver.quit()
 
 
-if __name__ == "__main__":
+def get_all_products():
     scraper = Scraper()
 
     scraper.accept_cookies()
@@ -185,3 +151,7 @@ if __name__ == "__main__":
     )
 
     scraper.close_driver()
+
+
+if __name__ == "__main__":
+    get_all_products()
